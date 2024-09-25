3805e89f4d2f229c94befa40dbfac55c589c484c
Leah Ball <leah.g.ball6@gmail.com>
2024-09-25 14:25:15 -0400

    adding prettier, bootstrap, and tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          You have successfully gotten to the start of Book-RAG!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Maybe add a button
       </header>
     </div>
   );
