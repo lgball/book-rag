93b73d3d7e3a9956a687710e8212ba0b53cc23ad
Leah Prince <leahp1@umbc.edu>
2024-10-28 23:20:44 -0400

    added dummy upload button

--- src/App.js
+++ src/App.js
@@ -9,10 +9,43 @@ function App() {
           You have successfully gotten to the start of Book-RAG!
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        Maybe add a button
+        <form action="upload-pdf">
+          <input type="file" id="myFile" name="filename" accept=".pdf"></input>
+          <button onclick="uploadFile()">Submit</button>
+          
+          const pdf = document.getElementById('myFile');
+
+          const fileData = new FormData();
+          fileData.append('pdf', pdf.files[0]);
+
+          fetch('http://localhost:5000/upload-pdf', {
+            method: 'POST',
+            body: fileData
+          })
+        </form>
       </header>
     </div>
   );
+}
+
+
+async function uploadFile() {
+  
+
+  const fileData = new FormData();
+  fileData.append('pdf', pdf.files[0]);
+
+  try {
+    const response = await fetch('http://localhost:5000/upload-pdf', {
+      method: 'POST',
+      body: fileData
+    });
+
+    const result = await response.json();
+    alert(result.message);
+  } catch (error) {
+    console.error("Error uploading image:", error);
+  }
 }
 
 export default App;
