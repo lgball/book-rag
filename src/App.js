6a04747b5fd46d1830217cd34aa347b45c2ffed0
Leah Prince <leahp1@umbc.edu>
2024-10-29 11:50:31 -0400

    added upload button

--- src/App.js
+++ src/App.js
@@ -11,41 +11,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <form action="upload-pdf">
           <input type="file" id="myFile" name="filename" accept=".pdf"></input>
-          <button onclick="uploadFile()">Submit</button>
-          
-          const pdf = document.getElementById('myFile');
-
-          const fileData = new FormData();
-          fileData.append('pdf', pdf.files[0]);
-
-          fetch('http://localhost:5000/upload-pdf', {
-            method: 'POST',
-            body: fileData
-          })
         </form>
       </header>
     </div>
   );
-}
-
-
-async function uploadFile() {
-  
-
-  const fileData = new FormData();
-  fileData.append('pdf', pdf.files[0]);
-
-  try {
-    const response = await fetch('http://localhost:5000/upload-pdf', {
-      method: 'POST',
-      body: fileData
-    });
-
-    const result = await response.json();
-    alert(result.message);
-  } catch (error) {
-    console.error("Error uploading image:", error);
-  }
 }
 
 export default App;
