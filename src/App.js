a6c53723de863fdc2e1aec0ba46d5da258f19356
Leah Prince <leahp1@umbc.edu>
2024-11-03 13:41:40 -0500

    added missing file error catch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [file, setFile] = useState();
+  const [responseText, setResponseText] = useState('');
+
+  function handleChange(event) {
+    setFile(event.target.files[0])
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    const url = 'http://localhost:5000/upload-pdf';
+    const formData = new FormData();
+
+    if (!file) {
+      setResponseText("No file selected! Please select a file!")
+    }
+    else {
+      formData.append('file', file);
+      formData.append('fileName', file.name);
+
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      };
+      
+
+      axios.post(url, formData, config).then((response) => {
+        if (response.data.error) {
+          setResponseText(`File "${file.name}" was empty. Please upload a different file!`)
+        } 
+        else {
+          setResponseText(`File "${file.name}" was uploaded successfully!`);
+        }
+        
+      })
+        .catch((error) => {
+          setResponseText(`${error.response.data.error}: File "${file.name}" was empty. Please upload a different file!`)
+      });
+    } 
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +52,11 @@ function App() {
           You have successfully gotten to the start of Book-RAG!
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <form action="upload-pdf">
-          <input type="file" id="myFile" name="filename" accept=".pdf"></input>
+        <form onSubmit={handleSubmit}>
+          <input type="file" id="myFile" name="filename" accept=".pdf" onChange={handleChange}></input>
+          <button type="submit">Upload PDF</button>
         </form>
+        <p>{responseText}</p>
       </header>
     </div>
   );
